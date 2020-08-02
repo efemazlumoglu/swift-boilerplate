ce0acebfcb86af99c9da01394a6beb4a1908db42
efemaz <efe.mazlumoglu@gmail.com>
2020-08-02 19:25:09 +0300

    initial commit

---
+++ Swift-BoilerPlate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-BoilerPlate
+//
+//  Created by Efe Mazlumoğlu on 2.08.2020.
+//  Copyright © 2020 Efe Mazlumoğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
