e54fc7d7105f6bb8de7413c02e7e67811ef5919e
efemazlumoglu <efe.mazlumoglu@gmail.com>
2020-08-02 19:29:22 +0300

    view controller 2 initial

---
+++ Swift-BoilerPlate/ViewController2.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController2.swift
+//  Swift-BoilerPlate
+//
+//  Created by Efe Mazlumoğlu on 2.08.2020.
+//  Copyright © 2020 Efe Mazlumoğlu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController2: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
